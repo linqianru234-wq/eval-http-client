5112aaf78b735b9e4e6c2eb0a1dc78ebf70bc25d
linqianru234-wq <linqianru234@gmail.com>
2026-04-03 15:29:38 +0800

    add file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='http-client',
+    version='1.0.0',
+    description='A utility library for common operations',
+    packages=['http-client'],
+    install_requires=[],
+)
